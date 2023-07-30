5e6a97f2636706b3efaded83a7a257fa04e36979
Mani A <mani.msd1357@gmail.com>
2023-07-30 19:14:51 +0530

    content to speak

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,5 +1,7 @@
 import pyttsx3
 engine = pyttsx3.init()
+engine.say("Hello how are you")
+engine.runAndWait()
 
 print("-----Speed Synthesis Multiplication Table-----")
 n = input("Enter a number: ")
