3ffefcff98092e7fa57b91d27a07f4fa7ff68491
Mani A <mani.msd1357@gmail.com>
2023-07-30 18:54:17 +0530

    creating a dictionary

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,3 +2,15 @@ print("-----Speed Synthesis Multiplication Table-----")
 n = input("Enter a number: ")
 # 9
 # one nines are nine
+dic = {
+    "1" : "ones",
+    "2" : "twos",
+    "3" : "threes",
+    "4" : "fours",
+    "5" : "fives",
+    "6" : "sixes",
+    "7" : "sevens",
+    "8" : "eights",
+    "9" : "nines",
+    "10" : "tens"
+}
