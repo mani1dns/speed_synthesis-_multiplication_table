041a75e69be2e3014762a6fc14e27bd8d9eb5ced
Mani A <mani.msd1357@gmail.com>
2023-07-30 19:06:13 +0530

    want to iterate from 1 to 10 so in for loop

--- multiplication_table.py
+++ multiplication_table.py
@@ -3,15 +3,22 @@ n = input("Enter a number: ")
 # 9
 # one nines are nine
 dic = {
-    "1" : "ones",
-    "2" : "twos",
-    "3" : "threes",
-    "4" : "fours",
-    "5" : "fives",
-    "6" : "sixes",
-    "7" : "sevens",
-    "8" : "eights",
-    "9" : "nines",
-    "10" : "tens"
+    "1": "ones",
+    "2": "twos",
+    "3": "threes",
+    "4": "fours",
+    "5": "fives",
+    "6": "sixes",
+    "7": "sevens",
+    "8": "eights",
+    "9": "nines",
+    "10": "tens"
 }
 txt = ""
+
+for i in range(1,11):
+    # one nines are nine
+    # two nines are eighteen
+    mul = i * int(n)
+    txt += str(i) + " " + dic[n] + " are " + str(mul) + "\n"
+print(txt)
